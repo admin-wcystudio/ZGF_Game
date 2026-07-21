5f5302b9c3d4235110707e4db663819303c6aafa
YoyoDev0424 <admin@wcystudio.com>
2026-07-21 19:57:32 +0800

    update game result scene

--- src/scenes/GameResultScene.js
+++ src/scenes/GameResultScene.js
@@ -110,7 +110,7 @@ export class GameResultScene extends Phaser.Scene {
         const playerName = this.playerInfo ? this.playerInfo.name : "玩家 1";
         this.game.renderer.snapshot((image) => {
             const link = document.createElement('a');
-            link.setAttribute('download', `愛蓮說_${playerName}_${new Date().getTime()}.png`);
+            link.setAttribute('download', `鄒忌諷齊王納諫_${playerName}_${new Date().getTime()}.png`);
             link.setAttribute('href', image.src);
             document.body.appendChild(link);
             link.click();
